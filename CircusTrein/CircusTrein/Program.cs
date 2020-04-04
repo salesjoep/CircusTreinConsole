bab2f00de3b1690c9760b499c129a5c6cbc75cc8
Luca Bulles <lucabulles@gmail.com>
2020-04-04 14:27:34 +0200

    Menu in method

--- CircusTrein/CircusTrein/Program.cs
+++ CircusTrein/CircusTrein/Program.cs
@@ -6,9 +6,34 @@ namespace CircusTrein
     {
         static void Main(string[] args)
         {
-            //Menu
-            Console.WriteLine("Welkom bij het programma Circus Trein!");
-            Console.WriteLine("Geef de aantallen van de dieren hieronder door:");
+            void Menu()
+            {
+                string menukeuze;
+
+                //Menu
+                Console.WriteLine("Welkom bij het programma Circus Trein!");
+                Console.WriteLine("Geef gegevens van de dieren hieronder door:\n");
+
+                Console.WriteLine("Toets 1 voor vleeseters, toets 2 voor planteneters en toets 3 voor gemengd.\n");
+                menukeuze = Console.ReadLine();
+                if (menukeuze == "1")
+                {
+                    Console.WriteLine("U heeft gekozen voor alleen vleeseters");
+                    Console.ReadLine();
+                }
+                else if (menukeuze == "2")
+                {
+                    Console.WriteLine("U heeft gekozen voor alleen planteneters");
+                    Console.ReadLine();
+                }
+                else if (menukeuze == "3")
+                {
+                    Console.WriteLine("U heeft gekozen voor gemengd");
+                    Console.ReadLine();
+                }
+            }
+            //Start van het programma
+            Menu();
 
         }
     }
