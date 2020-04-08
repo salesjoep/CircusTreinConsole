f278a9fb9cf7cfdb4f258c0b6bd88e6ff44271c2
Joep Sales <436499@student.fontys.nl>
2020-04-08 17:36:02 +0200

    Added code to Animal.cs and Wagon.cs

--- CircusTrein/CircusTrein/Animals.cs
+++ CircusTrein/CircusTrein/Animals.cs
@@ -4,15 +4,34 @@ using System.Text;
 
 namespace CircusTrein
 {
-    class Animals
+    //Enums om te gebruiker in de namespace, maar buiten de klasse
+    public enum AnimalDiet { Carnivore, Herbivore }
+    public enum AnimalSize { Small, Medium, Large }
+    public class Animals
     {
-        //Carnivores
-        int SmallCarnivore;
-        int MediumCarnivore;
-        int BigCarnivore;
-        //Herbivores
-        int SmallHerbivore;
-        int MediumHerbivore;
-        int BigHerbivore;
+        //Class Fields
+
+        //Class Properties
+        public string Name { get; set; }
+        public AnimalDiet Diet { get; set; }
+        public AnimalSize Size { get; set; }
+
+        //Class Constructors
+        public Animals(string name, AnimalSize size, AnimalDiet diet)
+        {
+            this.Name = name;
+            this.Size = size;
+            this.Diet = diet;
+
+        }
+
+        //Class Methods
+        public int RetrieveSize()
+        {
+            if (this.Size == AnimalSize.Small) return 1;
+            if (this.Size == AnimalSize.Medium) return 3;
+            if (this.Size == AnimalSize.Large) return 5;
+            return 1;
+        }
     }
 }
--- CircusTrein/CircusTrein/Wagon.cs
+++ CircusTrein/CircusTrein/Wagon.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace CircusTrein
 {
-    class Wagon
+    public class Wagon
     {
+        //Class Fields
+        private List<Animals> animals = new List<Animals>();
+        private int maxSize = 10;
+
+        //Class Properties
+        public bool IsWagonFull { get; set; }
+
+        //Class Constructors
+
+        //Class Methods
+        public bool CanBeAdded()
+        {
+            //Methode om regels op te stellen om dieren toe te voegen.
+            return false;
+        }
     }
 }
