e31f0d3fe947375b505ac5d40f27e910da93e170
Joep Sales <sales.joep@gmail.com>
2020-04-20 11:46:53 +0200

    Wagon is afgemaakt

--- CircusTrein/CircusTrein/Wagon.cs
+++ CircusTrein/CircusTrein/Wagon.cs
@@ -36,8 +36,34 @@ namespace CircusTrein
                 //Niet mogelijk
                 return false;
             }
-            //Methode om regels op te stellen om dieren toe te voegen.
-            return false;
+            //Als Diet Carnivore is en animalSize is >= animal die toegevoegd moet worden, return false
+            if (animals.FindIndex(index => index.Diet == AnimalDiet.Carnivore && index.Size >= animal.Size) >-1)
+            {
+                return false;
+            }
+            //Als Size <= Size van animal die toegevoegd moet worden, en zijn Diet is Carnivore, return false.
+            if (animals.FindIndex(index => index.Size <= animal.Size && animal.Diet == AnimalDiet.Carnivore) >-1)
+            {
+                return false;
+            }
+            //In elk ander geval, return true
+            return true;
+        }
+
+        //Methode om dieren toe te voegen aan animals lijst.
+        public bool InsertInWagon(Animals animal)
+        {
+            animals.Add(animal);
+            if (RetrieveLeftOverSize() <= 0)
+            {
+                IsWagonFull = true;
+            }
+            return true;
+        }
+
+        public List<Animals> RetrieveAnimals()
+        {
+            return animals;
         }
     }
 }
