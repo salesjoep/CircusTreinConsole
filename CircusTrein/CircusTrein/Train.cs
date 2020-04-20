6b4de8d0a44076b195aad8167d362905a06fd2e2
Joep Sales <sales.joep@gmail.com>
2020-04-20 12:05:41 +0200

    Added Train Class

--- CircusTrein/CircusTrein/Train.cs
+++ CircusTrein/CircusTrein/Train.cs
@@ -1,10 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CircusTrein
 {
-    class Train
+    public class Train
     {
+        //Class Fields
+        private List<Wagon> wagons = new List<Wagon>();
+        private List<Animals> animalsToAdd = new List<Animals>();
+
+        //Class Properties
+
+        //Class Constructor
+        public Train()
+        {
+            ResetWagons();
+        }
+
+        //Class Methods
+        public void ResetWagons()
+        {
+            wagons.Clear();
+            wagons.Add(new Wagon());
+        }
+
+        public void InsertAnimalsToWagon()
+        {
+            ResetWagons();
+            //Sorteer van Small - Medium - Large
+            animalsToAdd = animalsToAdd.OrderByDescending(order => order.Size).ToList();
+
+            foreach (Animals animal in animalsToAdd)
+            {
+                //For every wagon in the wagons list
+                for (int i = 0; i < wagons.Count; i++)
+                {
+                    if (wagons[i].IsWagonFull)
+                    {
+                        if (i == wagons.Count - 1)
+                        {
+                            wagons.Add(new Wagon());
+                            wagons[wagons.Count - 1].InsertInWagon(animal);
+                            break;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                    if (wagons[i].CanBeAdded(animal) == true)
+                    {
+                        wagons[i].InsertInWagon(animal);
+                        break;
+                    }
+                    if (i == wagons.Count - 1)
+                    {
+                        wagons.Add(new Wagon());
+                        wagons[wagons.Count - 1].InsertInWagon(animal);
+                        break;
+                    }
+                }
+            }
+        }
+
+        public List<Wagon> RetrieveWagons()
+        {
+            return wagons;
+        }
     }
 }
